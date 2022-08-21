9787e7319ada2d800fbc073df19e781093e3e036
L1tOveC <ujusenis@yandex.ru>
2022-08-21 19:46:35 +0500

    Изменения, которые будут включены в коммит: новый файл: data.js - моковые вопросы

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    question: "Почему а главное...",
+    incorrectAnswers: [
+      "ЗАЧЕМ",
+      "ПО ЧЕМУ",
+      "ЗАЧЕЕЕЕМ ",
+    ],
+    correctAnswer: "зачем, Игорь",
+  },
+
+  {
+    question:
+      "Сколько персонажей в Доте меняют форму атаки(с дальника на ближника и наоборот)?",
+    incorrectAnswers: [
+      "2",
+      "5",
+      "4",
+    ],
+    correctAnswer: "3",
+  }
+]
+
+export default questions;
