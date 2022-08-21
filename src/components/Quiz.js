005befe0ef95dd821ce427a4f1bc5a5a6075637d
L1tOveC <ujusenis@yandex.ru>
2022-08-21 22:15:33 +0500

    Применен хук UseState в качестве счётчика

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,19 @@
 import Questions from "./Questions";
+import {useState} from "react";
+
 const Quiz = () => {
+    const [CurrentQuestionIndex , setCurrentQuestionIndex] = useState(0);
+    console.log('result', CurrentQuestionIndex);
     return (
         <div className="quiz">
+            {CurrentQuestionIndex}
             <div>
                 <div className="score">Question 1/8</div>
                 <Questions />
-                <div className="next-button"> Next question</div>
+                <div className="next-button"
+                     onClick={() => setCurrentQuestionIndex(CurrentQuestionIndex +1)}>
+                    Next question
+                </div>
             </div>
         </div>);
 };
